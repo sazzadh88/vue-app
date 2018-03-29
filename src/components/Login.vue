b0284d84cb5cb2384ef97e5ad93b6a2111c3f648
sazzadh88 <sazzadh88@gmail.com>
2018-03-29 21:53:56 +0530

    Vue update

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,13 +1,68 @@
 <template>
     <div>
-        Welcome LOGIN
+        <v-container grid-list-md text-xs-center class="pad2">
+            <v-layout row>
+                <v-flex xs12>
+                    <v-app class="pad2">
+                        <v-form v-model="valid" ref="form" lazy-validation>
+                            <v-text-field label="Name" v-model="name" :rules="nameRules" :counter="10" required></v-text-field>
+                            <v-text-field label="E-mail" v-model="email" :rules="emailRules" required></v-text-field>
+                            <v-select label="Item" v-model="select" :items="items" :rules="[v => !!v || 'Item is required']" required></v-select>
+                            <v-checkbox label="Do you agree?" v-model="checkbox" :rules="[v => !!v || 'You must agree to continue!']" required></v-checkbox>
+
+                            <v-btn @click="submit" :disabled="!valid">
+                                submit
+                            </v-btn>
+                            <v-btn @click="clear">clear</v-btn>
+                        </v-form>
+                    </v-app>
+                </v-flex>
+            </v-layout>
+        </v-container>
     </div>
 </template>
 
 <script>
+    import axios from 'axios';
+
     export default {
-        created () {
-            console.log('hits form LOGIN');
+        data: () => ({
+            valid: true,
+            name: '',
+            nameRules: [
+                v => !!v || 'Name is required',
+                v => (v && v.length <= 10) || 'Name must be less than 10 characters'
+            ],
+            email: '',
+            emailRules: [
+                v => !!v || 'E-mail is required',
+                v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+            ],
+            select: null,
+            items: [
+                'Item 1',
+                'Item 2',
+                'Item 3',
+                'Item 4'
+            ],
+            checkbox: false
+        }),
+
+        methods: {
+            submit() {
+                    if (this.$refs.form.validate()) {
+                        // Native form submission is not yet supported
+                        axios.post('/api/submit', {
+                            name: this.name,
+                            email: this.email,
+                            select: this.select,
+                            checkbox: this.checkbox
+                        })
+                    }
+                },
+                clear() {
+                    this.$refs.form.reset()
+                }
         }
     }
 </script>
