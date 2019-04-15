69fd765046a645442c7a1672afcec44531ef4b95
Sazzad Hussain <sazzadh88@gmail.com>
2019-04-15 18:50:51 +0530

    no message

--- src/main.js
+++ src/main.js
@@ -2,27 +2,14 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import VueSweetAlert from 'vue-sweetalert'
- 
 
-// import Vuetify from 'vuetify'
 import App from './App.vue'
 import router from './router'
 
-// import 'vuetify/dist/vuetify.min.css'
-
 Vue.use(VueRouter) 
 Vue.use(VueSweetAlert)
-// Vue.use(Vuetify)
-
-
-
-
-
 
 Vue.config.productionTip = false
-
-
-
 
 new Vue({
   render: h => h(App),
